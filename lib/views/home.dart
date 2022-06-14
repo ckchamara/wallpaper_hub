6c21f77505443778b673e50f5dff1818af0a9c4b
ckchamara <ckchamara22@gmail.com>
2022-06-15 00:30:55 +0530

    1 38

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -18,24 +18,24 @@ class _HomeState extends State<Home> {
   List<CatagoriesModel> catagories = [];
   List<WallpaperModel> wallpapers = [];
 
-
   getTrendingWallpapers() async {
-    var response =  await http.get(
-      Uri.parse('https://api.pexels.com/v1/curated?per_page=1'),
+    var response = await http.get(
+      Uri.parse('https://api.pexels.com/v1/curated'),
+      //https://api.pexels.com/v1/curated?per_page=8
       headers: {
-        HttpHeaders.authorizationHeader: '563492ad6f917000010000012c0052627ec94545a4f01fff5e520e97',
+        HttpHeaders.authorizationHeader:
+            '563492ad6f917000010000012c0052627ec94545a4f01fff5e520e97',
       },
     );
-    // print(response.body.toString());
-    Map<String,dynamic> jsonData =  jsonDecode(response.body);
+    print(response.body.toString());
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
     jsonData['photos'].forEach((element) {
       // WallpaperModel wallpaperModel = new WallpaperModel();
       WallpaperModel wallpaperModel = WallpaperModel.fromMap(element);
       wallpapers.add(wallpaperModel);
     });
 
-    setState((){});
-
+    setState(() {});
   }
 
   @override
@@ -53,51 +53,56 @@ class _HomeState extends State<Home> {
         title: brandname(),
         elevation: 0.0,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Color(0xfff5f8fd),
-                  borderRadius: BorderRadius.circular(30)),
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Search", border: InputBorder.none),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xfff5f8fd),
+                    borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Search", border: InputBorder.none),
+                      ),
                     ),
-                  ),
-                  Icon(Icons.search)
-                ],
+                    InkWell(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => ))
+                        },
+                        child: Container(child: Icon(Icons.search)))
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Container(
-              height: 80,
-              child: ListView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 24),
-                  itemCount: catagories.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return CatagoryTile(
-                        title: catagories[index].catagoriesName,
-                        imgUrl: catagories[index].imgUrl);
-                  }),
-            )
-          ],
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 80,
+                child: ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    itemCount: catagories.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return CatagoryTile(
+                          title: catagories[index].catagoriesName,
+                          imgUrl: catagories[index].imgUrl);
+                    }),
+              ),
+              wallpapersList(wallpapers: wallpapers, context: context)
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
 
 class CatagoryTile extends StatelessWidget {
   String imgUrl = "";
@@ -124,7 +129,10 @@ class CatagoryTile extends StatelessWidget {
           Container(
             height: 50,
             width: 100,
-            color: Colors.black26,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.black26,
+            ),
             alignment: Alignment.center,
             child: Text(
               title,
--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 import '../model/wallpaper_model.dart';
@@ -18,18 +20,24 @@ Widget brandname() {
   );
 }
 
-Widget WallpapersList({required List<WallpaperModel> wallpapers, ggg}) {
+Widget wallpapersList({required List<WallpaperModel> wallpapers, context}) {
   return Container(
     padding: EdgeInsets.symmetric(horizontal: 16),
     child: GridView.count(
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(),
       crossAxisCount: 2,
       childAspectRatio: 0.6,
       mainAxisSpacing: 6.0,
+      crossAxisSpacing: 6.0,
       children: wallpapers.map((wallpaper) {
         return GridTile(
-            child: Container(
-            child: Image.network(wallpaper.src.portrait)
-        ));
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(17),
+              child: Container(
+              child: Image.network(wallpaper.src.portrait, fit: BoxFit.cover),
+        ),
+            ));
       }).toList(),
     ),
   );
